19599e97dc0413f99a1a083d9f0edd9cb9c55c59
Kelly Bianca <kbas@ic.ufal.br>
2020-08-02 11:07:30 -0300

    tentando fazer o menu

--- chatbot.py
+++ chatbot.py
@@ -17,10 +17,17 @@ def send_help(message):
 def send_exit(message):
     bot.reply_to(message, "Obrigad@, ficamos felizes em ajudar você")
 
+@bot.message_handler(commands=['menu'])
+def send_menu(message):
+    bot_ia = aiml.Kernel()
+    bot_ia.learn('./files_XML/menu.xml')
+    saida = bot_ia.respond(message.text)
+    bot.reply_to(message, saida)
+
 @bot.message_handler(func=lambda m: True)
 def converse(message):
     bot_ia = aiml.Kernel()
-    bot_ia.learn('./bot-converse.xml')
+    bot_ia.learn('./files_XML/bot-enfase.xml')
     saída = bot_ia.respond(message.text)
     bot.reply_to(message, saída)
      # bot.reply_to(message, 'OI')
